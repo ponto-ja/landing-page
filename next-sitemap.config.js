4f335509b571ca1aa0d8c1f6186a5b04a70cfe75
tiagosan040 <tiagosan040@gmail.com>
2024-06-26 13:35:25 -0300

    chore: configure sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ponto-ja.com/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
