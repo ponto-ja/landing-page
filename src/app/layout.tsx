5974727f5622c0f70be4b749dab0e5930ec2886a
tiagosan040 <tiagosan040@gmail.com>
2024-07-29 16:28:26 -0300

    feat: add script to enable microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className={`${inter.variable} antialiased`}>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nf33a0jmon"); 
+          `,
+          }}
+        />
+      </head>
       <body className="w-full h-screen">{children}</body>
     </html>
   );
